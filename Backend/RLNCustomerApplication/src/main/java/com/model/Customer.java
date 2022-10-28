b05bcd20a2d1ce03e5ca0f6843d3b900ee771b2e
2000031860 <2000031860@kluniversity.in>
2022-10-29 00:16:37 +0530

    allmodels properties updated

--- Backend/RLNCustomerApplication/src/main/java/com/model/Customer.java
+++ Backend/RLNCustomerApplication/src/main/java/com/model/Customer.java
@@ -1,5 +1,14 @@
 package com.model;
 
-public class Customer {
-
+public class Customer 
+{
+   private   int Customer_id;
+   private int AccountNumber;
+   private String FirsrName;
+   private String LastName;
+   private String ContactNumber;
+   private String Emailid;
+   private String password;
+   private String Accounttype;
+   
 }
--- Backend/RLNCustomerApplication/src/main/java/com/model/Transaction.java
+++ Backend/RLNCustomerApplication/src/main/java/com/model/Transaction.java
@@ -1,5 +1,13 @@
 package com.model;
 
-public class Transaction {
-
+public class Transaction 
+{
+   private int Customer_id;
+   private long transctionid;
+   private String transcaitonid;
+   private  String transctiontype;
+   private  long recieverAccountNumber;
+   private long TransctionAmout;
+   private  String TranscationStatus;
+   
 }
