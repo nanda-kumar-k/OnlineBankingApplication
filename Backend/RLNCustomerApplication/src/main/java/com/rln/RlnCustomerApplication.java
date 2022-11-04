33e472388a16d1d0ef11c9cb02eb81392e133683
N-and-U <nandakumar.k05@gmail.com>
2022-11-04 13:16:32 +0530

    testing

---
+++ Backend/RLNCustomerApplication/src/main/java/com/rln/RlnCustomerApplication.java
@@ -0,0 +1,14 @@
+package com.rln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlnCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlnCustomerApplication.class, args);
+		System.out.println("RLN Customer Application Running...!!");
+	}
+
+}
