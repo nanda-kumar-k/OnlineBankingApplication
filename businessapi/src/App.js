4b42464790293f0ed3df85d1667472f2b1cd5637
N-and-U <nandakumar.k05@gmail.com>
2022-11-19 23:11:03 +0530

    business api paymentstatus pages added

--- businessapi/src/App.js
+++ businessapi/src/App.js
@@ -2,6 +2,8 @@ import CustomerAuth from "./Components/Authentication/CustomerAuth";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./Components/Navbar/Navbar";
 import PaymentConformation from "./Components/Authentication/PaymentConformation";
+import Successfull from "./Components/PaymentStatus/Successfull";
+import PaymentFailure from "./Components/PaymentStatus/PaymentFailure";
 function App() {
   return (
     <>
@@ -10,6 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<CustomerAuth/>} />
           <Route path="paymentconformation" element={<PaymentConformation/>} />
+          <Route path="successfull" element={<Successfull/>} />
+          <Route path="failure" element={<PaymentFailure/>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
       </Router>
     </>
